dae43f9fe0c5cc0f6cf69c86e7b5c75334ee817b
Erika Ito <etito@wisc.edu>
2018-08-01 15:30:42 -0400

    Write script for TW soil moisture data frame

---
+++ TW_Plots.R
@@ -0,0 +1,77 @@
+######################################################## LOAD LIBRARIES ##################################################################
+
+########################################################### LOAD DATA ####################################################################
+
+Raw<-read.csv("file:///C:/Users/erikai94/Documents/UMass/Tidmarsh/McInnis_SM_TW_Raw.csv")    
+# Assign column names
+colnames(Raw)<-c("Meas_No", "Distance_(cm)", "Permittivity_(mV)", "Probe_Soil_Moisture_(%)", "Comments")      
+
+########################################################### UPDATE MATRIX ####################################################################
+
+# Update the data frame so here are rows for every distance of 10 cm              
+# First, make a sequence from 0 to the maximum distance in raw data, by increments of 10
+ Distances<-seq(0, Raw[nrow(Raw),"Distance_(cm)"], 10)
+
+# Next, make new data frame with a row for every distance
+MoistureData<-as.data.frame(matrix(nrow=length(Distances),ncol=ncol(Raw)))
+colnames(MoistureData)<-colnames(Raw)  
+MoistureData[which(Distances%in%Raw[,"Distance_(cm)"]),"Permittivity_(mV)"]<-Raw[,"Permittivity_(mV)"]
+MoistureData[which(Distances%in%Raw[,"Distance_(cm)"]),"Probe_Soil_Moisture_(%)"]<-Raw[,"Probe_Soil_Moisture_(%)"] 
+MoistureData[which(Distances%in%Raw[,"Distance_(cm)"]),"Comments"]<-as.character(Raw[,"Comments"])
+# Only create Meas_No counts for the raw data points from the moisture probe          
+MoistureData[which(!(is.na(MoistureData[,"Permittivity_(mV)"]))),"Meas_No"]<-1:length(which(!(is.na(MoistureData[,"Permittivity_(mV)"]))))
+MoistureData[,"Distance_(cm)"]<-Distances
+ 
+# Clean up data frame                
+MoistureData[which(is.na(MoistureData[,"Comments"])),"Comments"]<-""
+                
+# Add a column of Permittivity in (V)
+MoistureData[,"Permittivity_(V)"]<-MoistureData[,"Permittivity_(mV)"]/1000
+
+# Add linearly interpolated values between probe measurement data for distance up to 8100 cm:
+# Identify which rows have data
+Measured<-which(!(is.na((MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"Permittivity_(V)"]))))
+# Identify clusters of blanks
+# Take difference of each measured element and element ahead
+# Can identify the start of data gaps as places where the next measured point is more than one element away
+# LastMeasured represent the last element before a patch of missing data
+LastMeasured<-Measured[which(diff(Measured)>1)]
+# FirstMeasured represent the first element after patch of missing data
+# This is the element in Measured AFTER the elements where the next data point are more than one element away
+FirstMeasured<-Measured[which(diff(Measured)>1)+1]
+                
+# Write a for loop that linearly interpolates values in these blank clusters 
+# Note that there are 36 clusters of blanks (length(FirstMeasured) = length(LastMeasured) = 36)
+# Create a column in MoistureData for interpolated values
+MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"Permittivitiy_interp_cm_(V)"]<-MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100), "Permittivity_(V)"]       
+for (i in 1:36){
+    MoistureData[LastMeasured[i]:FirstMeasured[i],"Permittivitiy_interp_cm_(V)"]<-seq(MoistureData[LastMeasured[i],"Permittivitiy_interp_cm_(V)"], MoistureData[FirstMeasured[i],"Permittivitiy_interp_cm_(V)"],length=length(LastMeasured[i]:FirstMeasured[i]))
+    }
+    
+# Add a column for soil moisture calculated for the non- interpolated and the linearly interpolated permittivity columns              
+a0<-1.600
+a1<-8.400
+MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"cm_Soil_Moisture_Calculated_(%)"]<-sapply(MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"Permittivity_(V)"], function(x, y, z) ((1+6.175*x+6.303*x^2-73.578*x^3+183.44*x^4-184.78*x^5+68.017*x^6)-y)/z, a0, a1)
+MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"cm_Soil_Moisture_Calculated_interp_(%)"]<-sapply(MoistureData[1:which(MoistureData[,"Distance_(cm)"]==8100),"Permittivitiy_interp_cm_(V)"], function(x, y, z) ((1+6.175*x+6.303*x^2-73.578*x^3+183.44*x^4-184.78*x^5+68.017*x^6)-y)/z, a0, a1)
+
+# Get moisture data for every meter
+# Create a vector for every meter (every 100 cm)
+m_Distances<-seq(0, max(MoistureData[,"Distance_(cm)"]), 100) 
+# Create a vector for the rows for every meter of data
+m_Rows<-sapply(m_Distances, function(x) which(MoistureData[,"Distance_(cm)"]==x))
+               
+# Run the soil moisture calculation at the meter scale
+# Get moisture data for every meter
+# Create a vector for every meter (every 100 cm)
+m_Distances<-seq(0, max(MoistureData[,"Distance_(cm)"]), 100) 
+# Create a vector for the rows for every meter of data
+m_Rows<-sapply(m_Distances, function(x) which(MoistureData[,"Distance_(cm)"]==x))
+# Creat a vector of Permittivity values at the meter scale 
+m_Permittivity<-MoistureData[m_Rows,"Permittivity_(V)"]    
+
+# Add the soil moisture column to the data frame at the meter scale
+a0<-1.600
+a1<-8.400
+MoistureData[m_Rows,"m_Soil_Moisture_Calculated_(%)"]<-sapply(m_Permittivity, function(x, y, z) ((1+6.175*x+6.303*x^2-73.578*x^3+183.44*x^4-184.78*x^5+68.017*x^6)-y)/z, a0, a1)
+
+
